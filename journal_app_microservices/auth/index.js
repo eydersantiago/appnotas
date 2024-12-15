cd35ae799cc440fc811b3b9b8c6876540f9148a6
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2024-12-14 21:03:24 -0500

    monitoring

--- journal_app_microservices/auth/index.js
+++ journal_app_microservices/auth/index.js
@@ -2,18 +2,34 @@ import authRoutes from './routes/authRoutes.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
-import './firebase/config.js';
-
 import fetch from 'node-fetch';
-globalThis.fetch = fetch;
+import promClient from 'prom-client';
 
+globalThis.fetch = fetch;
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+// Registro global de métricas
+const register = new promClient.Registry();
+
+// Colección de métricas por defecto (GC, CPU, memoria, etc.)
+promClient.collectDefaultMetrics({
+    register,
+    prefix: 'auth_service_'
+});
+
+// Ruta de métricas
+app.get('/metrics', async (req, res) => {
+    try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+    } catch (ex) {
+        res.status(500).end(ex);
+    }
+});
 
 app.use('/auth', authRoutes);
 
